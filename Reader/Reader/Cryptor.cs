16137eb8045aac438b9d4db8faaeda7de66db9b1
Lubos Curgo <ext.lubos.curgo@tieto.com>
2020-12-06 16:02:13 +0100

    Version 3 - encrypted txt file

---
+++ Reader/Reader/Cryptor.cs
@@ -0,0 +1,22 @@
+﻿namespace Reader
+{
+    public class ReverseCryptor : ICryptor
+    {
+        public string Decrypt(string content)
+        {
+            string result = string.Empty;
+
+            for (int i = content.Length - 1; i >= 0; i--)
+            {
+                result += content[i];
+            }
+
+            return result;
+        }
+    }
+
+    public interface ICryptor
+    {
+        string Decrypt(string content);
+    }
+}
--- Reader/Reader/Program.cs
+++ Reader/Reader/Program.cs
@@ -8,26 +8,53 @@ namespace Reader
         private static void Main(string[] args)
         {
             var filePath = GetFilePath();
-            ReadAndDumpFile(filePath);
+
+            var fileContent = GetFileContent(filePath);
+
+            if (String.IsNullOrEmpty(fileContent))
+            {
+                return;
+            }
+
+            var isFileEncrypted = IsFileEncrypted();
+
+            if (isFileEncrypted)
+            {
+                ICryptor cryptor = new ReverseCryptor();
+                fileContent = cryptor.Decrypt(fileContent);
+            }
+
+            DumpFileContentToScreen(fileContent);
 
             Console.ReadKey();
         }
 
-        private static void ReadAndDumpFile(string filePath)
+        private static bool IsFileEncrypted()
+        {
+            Console.WriteLine("Is file encrypted? type: Y for yes, N for no");
+            var key = Console.ReadLine();
+            return key == "Y";
+        }
+
+        private static void DumpFileContentToScreen(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("File content bellow");
+            Console.WriteLine();
+            Console.WriteLine(text);
+        }
+
+        private static string GetFileContent(string filePath)
         {
             if (!File.Exists(filePath))
             {
                 Console.WriteLine($"Specified file do not exist in directory {GetFileDir()}");
                 Console.WriteLine("Exiting application.");
-                return;
+                return string.Empty;
             }
 
             Console.WriteLine($"Reading file in location: {filePath}");
-            string text = File.ReadAllText(filePath);
-
-            Console.WriteLine("File content bellow");
-            Console.WriteLine();
-            Console.WriteLine(text);
+            return File.ReadAllText(filePath);
         }
 
         private static string GetFilePath()
@@ -40,6 +67,7 @@ namespace Reader
             Console.Write($"Please provide file extension (e.g. .xml or .txt)");
             Console.WriteLine();
             var fileExtension = Console.ReadLine();
+
             return Path.Combine(fileDir, fileName + fileExtension);
         }
 
